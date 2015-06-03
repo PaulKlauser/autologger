60e218c36a9fcd89d78c29dac7199391f4076f57
muppetman <paul.klauser@gmail.com>
2015-06-03 19:54:19 -0400

    Edmunds connection working!

--- app/src/main/java/paulklauser/gastracker/ui/addcar/EnterNameFragment.java
+++ app/src/main/java/paulklauser/gastracker/ui/addcar/EnterNameFragment.java
@@ -6,7 +6,12 @@ import android.support.annotation.Nullable;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
 import android.widget.AutoCompleteTextView;
+import android.widget.BaseAdapter;
+import android.widget.TextView;
+
+import java.util.ArrayList;
 
 import paulklauser.gastracker.R;
 import paulklauser.gastracker.network.EdmundsHelper;
@@ -20,15 +25,23 @@ public class EnterNameFragment extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         View rootView = inflater.inflate(R.layout.fragment_add_car_info, container, false);
-        EdmundsHelper helper = new EdmundsHelper();
-        helper.loadMakesFromWeb();
+
         return rootView;
     }
 
     @Override
     public void onViewCreated(View view, Bundle savedInstanceState) {
         super.onViewCreated(view, savedInstanceState);
-        AutoCompleteTextView make = (AutoCompleteTextView) view.findViewById(R.id.make);
+        final AutoCompleteTextView make = (AutoCompleteTextView) view.findViewById(R.id.make);
 
+        EdmundsHelper helper = new EdmundsHelper();
+        helper.loadMakesFromWeb((AddCarActivity) getActivity(), new EdmundsHelper.EdmundsHelperListener() {
+            @Override
+            public void onWebRequestComplete(ArrayList<String> carMakes) {
+                make.setAdapter(new ArrayAdapter<>(getActivity(), android.R.layout.simple_dropdown_item_1line,
+                        carMakes));
+            }
+        });
     }
+
 }
